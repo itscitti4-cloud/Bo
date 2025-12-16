8e7e69b6a7f72fcc9948d8f5a11688a058fcf92f
itscitti4-cloud <itscitti4@gmail.com>
2025-12-17 03:08:33 +0600

    Update gs.js

--- scripts/cmds/gs.js
+++ scripts/cmds/gs.js
@@ -9,7 +9,7 @@ module.exports = {
     aliases: ["gs", "market", "cmdstore"],
     version: "0.0.1",
     role: 2,
-    author: "ArYAN",
+    author: "AkHi",
     shortDescription: {
       en: "📌 Goatstore - Your Command Marketplace"
     },
