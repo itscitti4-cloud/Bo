812af9428b7cfbc0cb25aad4581435f91ba450e0
itscitti4-cloud <itscitti4@gmail.com>
2025-12-17 23:05:38 +0600

    Create offbot.js

---
+++ scripts/cmds/offbot.js
@@ -0,0 +1,20 @@
+module.exports = {
+ config: {
+ name: "offbot",
+ version: "1.0",
+ author: "AkHi",
+ countDown: 45,
+ role: 2,
+ shortDescription: "Turn off bot",
+ longDescription: "Turn off bot",
+ category: "owner",
+ guide: "{p}{n}"
+ },
+ onStart: async function ({event, api}) {
+ const permission = [ "61583939430347" ];
+ if (!permission.includes(event.senderID)) {
+ api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nYou don't have permission to use this command.\n═══ஜ۩۞۩ஜ═══╝", event.threadID, event.messageID);
+ return;
+ }
+ api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nsuccessfully Turned Off System ✅\═══ஜ۩۞۩ஜ═══╝",event.threadID, () =>process.exit(0))}
+};
