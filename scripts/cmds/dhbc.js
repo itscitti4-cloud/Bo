454e863af8c225a2e6adea86ec4eb81389fd0bb1
itscitti4-cloud <itscitti4@gmail.com>
2025-12-24 17:08:00 +0600

    Update dhbc.js

--- scripts/cmds/dhbc.js
+++ scripts/cmds/dhbc.js
@@ -78,4 +78,10 @@ function formatText(text) {
 		.toLowerCase()
 		.replace(/[\u0300-\u036f]/g, "")
 		.replace(/[đ|Đ]/g, (x) => x == "đ" ? "d" : "D");
+	
+	// টাকা বিয়োগ বা যোগ করার পর এভাবে সেভ করতে হয়
+await usersData.set(userID, { 
+    money: newBalance 
+});
+	
 }
